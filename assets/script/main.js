8421c4e5315c42a11453ffe6198f5a900c5b9f8a
rafaelcarbono <rafael.oliveiradesena@outlook.com>
2021-11-24 15:15:24 -0300

    Teste

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,6 @@
-let question = document.querySelectorAll(".question");
+let questionList = document.querySelectorAll(".question");
 let questions = [];
 let answers = [];
-let options = [];
 let showq;
 let count = 0;
 let trocar = document.getElementById("trocar");
@@ -9,30 +8,32 @@ let iniciar = document.getElementById("iniciar");
 let contagem = document.getElementById("count");
 
 class CreatePerfil {
+
     constructor(name, description, valor) {
+
         this.name = name;
+
         this.description = description;
+
         this.valor = valor;
-        this.incre = function(){
-            valor = valor + 4;
-        }
+
     }
 }
 
-let perfil1 = new CreatePerfil("Diretivo","descrição",0);
-let perfil2 = new CreatePerfil("Interativo","descrição",0);
-let perfil3 = new CreatePerfil("Social","descrição",0);
-let perfil4 = new CreatePerfil("Calculista","descrição",0);
+    let perfil1 = new CreatePerfil("Diretivo","descrição",0);
 
-while (question[count] != undefined) {
-    questions.push(question[count]);
-        if(questions[count].hasChildNodes()){
-            let qoptions = questions[count].children;
-                for (var i = 0; i < qoptions.length; i++) {
-                    let option = qoptions[i].firstElementChild;
-                    options.push(option);
-                }count +=1;
-        }
+    let perfil2 = new CreatePerfil("Interativo","descrição",0);
+
+    let perfil3 = new CreatePerfil("Social","descrição",0);
+
+    let perfil4 = new CreatePerfil("Calculista","descrição",0);
+
+
+while (questionList[count] != undefined) {
+
+    questions.push(questionList[count]);
+    count +=1;
+    
 }
 
 function randomQ (min, max){
@@ -73,18 +74,18 @@ function addPerfil(){
         perfil1.valor+=4;
 
     }else if(choice.value == "option2"){
- 
+
         perfil2.valor+=4;
 
 
     }else if(choice.value == "option3"){
- 
+
         perfil3.valor+=4;
 
     }else{
         perfil4.valor+=4;
     }
-    
+
     console.log(perfil1.name, perfil1.valor, perfil2.name, perfil2.valor, perfil3.name, perfil3.valor, perfil4.name, perfil4.valor);
 
 }
@@ -101,34 +102,38 @@ function changeQuestion() {
         
     if(questions.length > 0){
         questions[showq].classList.remove("hidden");
-    }else{
-
+    }
+    else{
         if(perfil1.valor > Math.max(perfil2.valor,perfil3.valor,perfil4.valor)){
             console.log("Você tem o Perfil " + perfil1.name);
+
         }else if(perfil2.valor > Math.max(perfil1.valor,perfil3.valor,perfil4.valor)){
             console.log("Você tem o Perfil " + perfil2.name);
+
         }else if(perfil3.valor > Math.max(perfil1.valor,perfil2.valor,perfil4.valor)){
             console.log("Você tem o Perfil " + perfil3.name);
+
         }else if(perfil4.valor > Math.max(perfil1.valor,perfil2.valor,perfil3.valor)){
             console.log("Você tem o Perfil " + perfil4.name);
         }
-
-        console.log("finish");
-            trocar.classList.remove("trocar");
-            trocar.classList.add("hidden");
-            contagem.innerHTML = "Acabou!!!";
+        trocar.classList.remove("trocar");
+        trocar.classList.add("hidden");
+        contagem.innerHTML = "Acabou!!!";
     }
 
+    
   }
- 
-  
+    starter();
+    change();
 
-
-
-iniciar.onclick = () => {
-    start();
+function starter() {
+    iniciar.onclick = () => {
+        start();
+    };
 }
 
-trocar.onclick = () => {
-    changeQuestion();
-};
+function change() {
+    document.onclick = () => {
+        changeQuestion();
+    };
+}
