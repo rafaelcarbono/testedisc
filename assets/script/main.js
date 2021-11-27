4ee5dada428b804ca08111c2696ed1c1180ef734
rafaelcarbono <rafael.oliveiradesena@outlook.com>
2021-11-27 00:25:02 -0300

    Corrigindo erro de valores de perfil duplicados, resultado final sem possibilidade de empate

--- assets/script/main.js
+++ assets/script/main.js
@@ -45,45 +45,68 @@ function start(){
 
 function addPerfil(){
 
-    for(i = 0; i < perfis.length; i++){
-        perfis[i].valor+=1;
-    }
-    
     let nameChoice = questions[showq].id;
     let choice = questions[showq].querySelector('input[name='+ nameChoice + ']:checked').value;
+    let incre;
+    let incretwo;
 
+    if (questions.length > 1){
+        incre = 1;
+        incretwo = 4;
+    }else{
+        incre = 2;
+        incretwo = 5 * incre;
+    }
+
+    for(i = 0; i < 4; i++){
+        perfis[i].valor = perfis[i].valor + incre;
+    }
+    
     switch(choice){
         case choice = "option1":
-            perfis[0].valor+=4;
+            perfis[0].valor = perfis[0].valor + incretwo;
         break;
         case choice = "option2":
-            perfis[1].valor+=4;
+            perfis[1].valor = perfis[1].valor + incretwo;
         break;
         case choice = "option3":
-            perfis[2].valor+=4;
+            perfis[2].valor = perfis[2].valor + incretwo;
         break;
         case choice = "option4":
-            perfis[3].valor+=4;
+            perfis[3].valor = perfis[3].valor + incretwo;
         break;
     }
+    console.log(perfis[0].valor, perfis[1].valor, perfis[2].valor, perfis[3].valor);
 
 }
 
 function calPerfil(){
+
+
+
     if(questions.length != 0){
         questions[showq].classList.remove("hidden");
 
     }   else{
             document.getElementById("container").classList.add("hidden");
-                if(perfis[0].valor > Math.max(perfis[1].valor,perfis[2].valor,perfis[3].valor)){
-                    document.getElementById("p01").classList.remove("hidden");
-                }if(perfis[1].valor > Math.max(perfis[0].valor,perfis[2].valor,perfis[3].valor)){
-                    document.getElementById("p02").classList.remove("hidden");
-                }if(perfis[2].valor > Math.max(perfis[0].valor,perfis[1].valor,perfis[3].valor)){
-                    document.getElementById("p03").classList.remove("hidden");
-                }if(perfis[3].valor > Math.max(perfis[0].valor,perfis[1].valor,perfis[2].valor)){
-                    document.getElementById("p04").classList.remove("hidden");
+
+                let res = Math.max(perfis[0].valor, perfis[1].valor, perfis[2].valor, perfis[3].valor);
+
+                switch(res){
+                    case res = perfis[0].valor:
+                        document.getElementById("p01").classList.remove("hidden");
+                    break;
+                    case res = perfis[1].valor:
+                        document.getElementById("p02").classList.remove("hidden");
+                    break;
+                    case res = perfis[2].valor:
+                        document.getElementById("p03").classList.remove("hidden");
+                    break;
+                    case res = perfis[3].valor:
+                        document.getElementById("p04").classList.remove("hidden");
+                    break;
                 }
+
     }    
 }
 
